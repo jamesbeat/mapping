5f6001e40d5756cd99152fea5e34199da54dc3dd
Sebastian Ross <bastiross@gmail.com>
2014-02-10 14:02:08 +0100

    include map

---
+++ profilemap.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset=utf-8 />
+<title>Load GeoJSON</title>
+
+<meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no' />
+
+<link href='css/style.css' rel='stylesheet' />
+
+<script type="text/javascript" src="//use.typekit.net/zns6amn.js"></script>
+<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+
+<script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
+<script src='https://api.tiles.mapbox.com/mapbox.js/v1.6.1/mapbox.js'></script>
+<link href='https://api.tiles.mapbox.com/mapbox.js/v1.6.1/mapbox.css' rel='stylesheet' />
+<script src='https://api.tiles.mapbox.com/mapbox.js/plugins/leaflet-osm/v0.1.0/leaflet-osm.js'></script>
+
+
+<script src="js/leaflet.geometryutil.js"></script>
+
+
+<script type='text/javascript' src='js/tinycolor.js'></script>
+<script type='text/javascript' src='js/raphael-min.js'></script>
+<script type='text/javascript' src='js/profile.js'></script>
+<script type='text/javascript' src='js/mapper.js'></script>
+</head>
+<body>
+
+
+
+
+
+<div id="graphic_container">
+
+<div id="map_container">
+	<div id='map'></div>
+</div>
+
+<div id="mask"></div>
+
+<div id="profile_container">
+	<div id='profile'></div>
+	
+	<div id="meta">
+		
+		
+		<h1>Passo Dello Stelvio</h1>
+		<h3>Via Glurns - Bormio</h3>
+		<h2>2.757 m</h2>
+		
+	</div>
+	
+</div>
+
+</div>
+
+
+</body>
+</html>
